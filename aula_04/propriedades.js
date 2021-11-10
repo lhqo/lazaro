5269e619b20a6a2bcdde7c9f96c5d6386b2aa89f
lhqo <lazarohenriqueqo@gmail.com>
2021-11-09 23:43:36 -0300

    add aula4

---
+++ aula_04/propriedades.js
@@ -0,0 +1,56 @@
+/*let frase = " nao sou o goku"
+console.log (frase.length)
+*/
+
+/*let frase = " nao sou o goku"
+console.log (frase)*/
+
+/*var senha = "12345678"
+if (senha.length == 8){
+    console.log('senha correta')
+}else if(senha.length < 4 && < 8){
+    console.log('nao segura')
+}else{
+    console.log('incorreta')
+}*/
+
+
+/*const input = require("readline-sync")
+
+console.log("Digite uma senha");
+
+const senha = input.question ('A senha deve ter oito digito');
+if (senha.length == 8){
+    console.log('senha correta')
+}else{
+    console.log('incorreta')
+}*/
+/*
+let salario = 1000
+while (salario < 5000){
+    salario += 100 
+console.log("o salario é" + salario)
+}*/
+
+/*let contador = 0
+while (contador < 50){
+    console.log ('repetição nº' + contador)
+    contador++
+}*/
+
+/*const linguagens = ['javascript', 'c#', 'python', 'php']
+let index = 0
+do{
+    console.log(linguagens[index])
+    index++
+}while (index < linguagens.length)
+*
+
+/*for (var i = 0; i < 9; i++){
+    console.log(i)
+}*/    
+
+for (var i = 0;; i++){
+    console.log(i)
+    if ( i>7 ) break
+}
